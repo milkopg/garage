2f843c0fad894517b351d243d4e5767c0f427702
Milko Galev <milkopg@yahoo.com>
2017-12-21 21:38:51 +0200

    Adding AppConfig classes

---
+++ src/main/java/com/development/configuration/AppConfig.java
@@ -0,0 +1,54 @@
+package com.development.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@Import(JpaConfiguration.class)
+@EnableWebMvc
+@ComponentScan(basePackages = "com.development")
+public class AppConfig extends WebMvcConfigurationSupport  {
+	
+
+	/**
+     * Configure InternalResourceViewResolver to deliver preferred views.
+     */
+	@Bean 
+	public InternalResourceViewResolver viewResolver() { 
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver(); 
+		resolver.setPrefix("/WEB-INF/views/"); 
+		resolver.setSuffix(".jsp"); 
+		return resolver; 
+	}
+	
+	/**
+     * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+    
+    /**
+     * Configure MessageSource to lookup any validation/error message in internationalized property files
+     */
+    @Bean
+	public MessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasename("messages");
+	    return messageSource;
+	}
+	
+	/*@Override 
+	public void addViewControllers(ViewControllerRegistry registry) { 
+		registry.addViewController("/").setViewName("home"); 
+	} */
+}
---
+++ src/main/sql/init_data.sql
@@ -0,0 +1,2 @@
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'user123';
+GRANT ALL PRIVILEGES ON * . * TO 'user'@'localhost';
