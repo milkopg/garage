0a0b13043f87ce4321bae760ac557dcac6a86d2e
Milko Galev <milkopg@yahoo.com>
2017-12-24 12:07:04 +0200

    Using latest mysql storage engine

--- src/main/java/com/development/configuration/JpaConfiguration.java
+++ src/main/java/com/development/configuration/JpaConfiguration.java
@@ -48,10 +48,10 @@ public class JpaConfiguration {
 
 	private Properties getJpaProperties() {
 		Properties properties = new Properties();
-		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
 		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
 		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
 		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+		properties.put("hibernate.dialect.storage_engine", environment.getRequiredProperty("hibernate.dialect.storage_engine"));
 		return properties;
 	}
 
