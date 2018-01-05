c83d9ad76fbf166a1e4fcbb89a5fd3175e31b648
Milko Galev <milkog@riscogroup.com>
2018-01-05 14:18:15 +0200

    Remove unused import

--- src/main/java/com/development/dao/OperationDao.java
+++ src/main/java/com/development/dao/OperationDao.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import com.development.model.Operation;
-import com.development.model.ParkingLevel;
 
 public interface OperationDao {
 	Operation getById(long id);
